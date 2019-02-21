be4dc970eaf7fca4d048c5b3539d79759e8d239f
ferdi <ferdireza96@gmail.com>
2019-02-21 12:48:11 +0700

    add some new variable

--- app/src/main/java/com/magentamedia/yaumiamal/models/Alarm.java
+++ app/src/main/java/com/magentamedia/yaumiamal/models/Alarm.java
@@ -3,7 +3,7 @@ package com.magentamedia.yaumiamal.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Alarm {
+public class Alarm implements Parcelable {
 
     private String AlarmLabel;
     private String AlarmText;
@@ -28,12 +28,6 @@ public class Alarm {
         this.time = time;
     }
 
-    protected Alarm(Parcel in) {
-        this.AlarmLabel = in.readString();
-        this.AlarmText = in.readString();
-        this.time = in.readLong();
-    }
-
     @Override
     public String toString() {
         return "alarm {" +
@@ -42,4 +36,34 @@ public class Alarm {
                 ", waktu = '" + time + '\'' +
                 '}';
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.AlarmLabel);
+        dest.writeString(this.AlarmText);
+        dest.writeLong(this.time);
+    }
+
+    protected Alarm(Parcel in) {
+        this.AlarmLabel = in.readString();
+        this.AlarmText = in.readString();
+        this.time = in.readLong();
+    }
+
+    public static final Parcelable.Creator<Alarm> CREATOR = new Parcelable.Creator<Alarm>() {
+        @Override
+        public Alarm createFromParcel(Parcel source) {
+            return new Alarm(source);
+        }
+
+        @Override
+        public Alarm[] newArray(int size) {
+            return new Alarm[size];
+        }
+    };
 }
