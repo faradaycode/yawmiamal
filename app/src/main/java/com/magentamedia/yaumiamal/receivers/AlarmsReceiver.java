3c4d5439c1d1be0363f2d6c840cabcdecd247531
ferdi <ferdireza96@gmail.com>
2019-02-21 12:47:35 +0700

    add set and cancel alarm manager

--- app/src/main/java/com/magentamedia/yaumiamal/receivers/AlarmsReceiver.java
+++ app/src/main/java/com/magentamedia/yaumiamal/receivers/AlarmsReceiver.java
@@ -1,5 +1,6 @@
 package com.magentamedia.yaumiamal.receivers;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -13,29 +14,35 @@ import android.widget.Toast;
 
 import com.magentamedia.yaumiamal.MainActivity;
 import com.magentamedia.yaumiamal.R;
+import com.magentamedia.yaumiamal.models.Alarm;
+import com.magentamedia.yaumiamal.services.ServIntentYaumi;
+
+import java.util.ArrayList;
 
 public class AlarmsReceiver extends BroadcastReceiver {
 
-    private static int REQUEST_ID = 880;
     private static String TAG = AlarmsReceiver.class.getSimpleName();
     private static String ALARMS_EXTRA = "alarms_extra";
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-//        ArrayList<Alarm> alarm = intent.getParcelableArrayListExtra(ServIntentYaumi.ALARMS_EXTRA);
+        ArrayList<Alarm> alarm = intent.getParcelableArrayListExtra(ServIntentYaumi.EXTRAS_DATA);
 
         String label = intent.getStringExtra("TITLE");
         String teks = intent.getStringExtra("CONTENT");
-        int a_id = intent.getIntExtra("A_ID", REQUEST_ID);
+        int a_id = intent.getIntExtra("A_ID", -1);
 
-        Log.wtf(TAG, label);
 
         Intent iin = new Intent(context, MainActivity.class);
 
         context.startActivity(iin);
 
-        Toast.makeText(context, label, Toast.LENGTH_LONG);
+        setMyAlarm(context, alarm);
+
+        Toast.makeText(context, label, Toast.LENGTH_LONG).show();
+
+        Log.wtf(TAG, alarm.toString());
 
     }
 
@@ -70,5 +77,47 @@ public class AlarmsReceiver extends BroadcastReceiver {
 
         assert nmanager != null;
         nmanager.notify(alarmID, mNotifyBuilder.build());
+    }
+
+    public static void setMyAlarm(Context context, ArrayList<Alarm> alarms) {
+
+        if (alarms.size() > 0) {
+
+            AlarmManager mgrAlarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+            //set alarm each data in arraylist
+            for (int i = 0; i < alarms.size(); i++) {
+                Intent alarmintent = new Intent(context, AlarmsReceiver.class);
+                alarmintent.putExtra("TITLE", alarms.get(i).getAlarmLabel());
+                alarmintent.putExtra("CONTENT", alarms.get(i).getAlarmText());
+                alarmintent.putExtra("A_ID", i);
+
+                PendingIntent pendingIntent = PendingIntent
+                        .getBroadcast(context, i, alarmintent, 0);
+
+                assert mgrAlarm != null;
+
+                if (System.currentTimeMillis() < alarms.get(i).getTime()) {
+                    mgrAlarm.setExact(AlarmManager.RTC_WAKEUP, alarms.get(i).getTime(), pendingIntent);
+                }
+            }
+
+            Log.wtf(TAG, alarms.toString());
+        }
+    }
+
+    public static void cancelAlarm(Context context, ArrayList<Alarm> alarms) {
+
+        Intent alarmintent = new Intent(context, AlarmsReceiver.class);
+
+        for (int i = 0; i < alarms.size(); i++) {
+            PendingIntent pendingIntent = PendingIntent
+                    .getBroadcast(context, i, alarmintent, 0);
+
+            AlarmManager mgrAlarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+            assert mgrAlarm != null;
+            mgrAlarm.cancel(pendingIntent);
+        }
     }
 }
