74b1b79afcce748b4a8591fd66e0b6050f5d2ca3
ferdi <ferdireza96@gmail.com>
2019-08-19 11:43:17 +0700

    rework 19-8-2019

---
+++ app/src/main/java/com/bangfer/assistask/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bangfer.assistask.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bangfer.assistask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
