63f8ee6190e0adfb2f3ff004994251dc54694d88
ferdi <ferdireza96@gmail.com>
2019-08-21 10:43:52 +0700

    fix some value resources

--- app/src/main/java/com/bangfer/assistask/Activities/MainActivity.java
+++ app/src/main/java/com/bangfer/assistask/Activities/MainActivity.java
@@ -1,16 +1,34 @@
+
+/*
+ * Created by Ferdi Reza on 8/20/19 9:52 AM
+ * Copyright (c) 2019 . All rights reserved
+ * Last modified 8/20/19 9:23 AM
+ */
+
 package com.bangfer.assistask.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.bangfer.assistask.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.bt_setting)
+    void toSetting()
+    {
+        startActivity(new Intent(MainActivity.this, PengaturanActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(MainActivity.this);
     }
 }
